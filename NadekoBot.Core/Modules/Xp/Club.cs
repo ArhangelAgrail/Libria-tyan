588e980519ec497b29f490a5a02f74966e61b42c
CuteAlien <CuteAlien@DESKTOP-KC00TBJ>
2021-09-17 16:17:16 +0300

    fix fixed

--- NadekoBot.Core/Modules/Xp/Club.cs
+++ NadekoBot.Core/Modules/Xp/Club.cs
@@ -779,7 +779,7 @@ namespace NadekoBot.Modules.Xp
 
 
             [NadekoCommand, Usage, Description, Aliases]
-            public Task ClubInvestLb(int page = 0)
+            public Task ClubInvestLb(int page = 1)
             {
                 var club = _service.GetClubByMember(Context.User);
 
@@ -813,7 +813,7 @@ namespace NadekoBot.Modules.Xp
                         .WithDescription(GetText("club_month_invests", String.Format("{0:#,0}", club.TotalCurrency), Bc.BotConfig.CurrencySign))
                         .WithFooter(GetText("club_invests_desc"))
                         .WithOkColor()
-                        .AddField(GetText("club_invests_members", (curPage + 1) * 1, club.Users.Count/((curPage + 1) * 10) < 1 ? club.Users.Count : (curPage + 1) * 10, club.Users.Count), Format.Bold(string.Join("\n", result.Skip(curPage * 10).Take(10))), false)
+                        .AddField(GetText("club_invests_members", (--curPage + 1) * 10, club.Users.Count/((--curPage + 1) * 10) < 1 ? club.Users.Count : (--curPage + 1) * 10, club.Users.Count), Format.Bold(string.Join("\n", result.Skip(--curPage * 10).Take(10))), false)
                         .AddField("_ _", GetText("club_total_invests", String.Format("{0:#,0}", total), Bc.BotConfig.CurrencySign));
                     return embed;
                 }, club.Users.Count, 10, addPaginatedFooter: false);
