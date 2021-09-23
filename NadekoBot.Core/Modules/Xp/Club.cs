576376e847e673656ab4d090c9792f266293a55d
CuteAlien <CuteAlien@DESKTOP-KC00TBJ>
2021-09-23 20:43:11 +0300

    .clubin + .events fix

--- NadekoBot.Core/Modules/Xp/Club.cs
+++ NadekoBot.Core/Modules/Xp/Club.cs
@@ -783,7 +783,7 @@ namespace NadekoBot.Modules.Xp
             {
                 var club = _service.GetClubByMember(Context.User);
 
-                if (--page < 0 || ++page > club.Users.Count/10)
+                if (--page < 0 || page > club.Users.Count/10)
                     return Task.CompletedTask;
 
                 if (club == null)
@@ -813,7 +813,7 @@ namespace NadekoBot.Modules.Xp
                         .WithDescription(GetText("club_month_invests", String.Format("{0:#,0}", club.TotalCurrency), Bc.BotConfig.CurrencySign))
                         .WithFooter(GetText("club_invests_desc"))
                         .WithOkColor()
-                        .AddField(GetText("club_invests_members", curPage > 1 ? curPage * 10 : curPage, club.Users.Count/((curPage + 1) * 10) < 1 ? club.Users.Count : (curPage + 1) * 10, club.Users.Count), Format.Bold(string.Join("\n", result.Skip((curPage - 1) * 10).Take(10))), false)
+                        .AddField(GetText("club_invests_members", curPage > 0 ? curPage * 10 : 1, club.Users.Count/((curPage + 1) * 10) < 1 ? club.Users.Count : (curPage + 1) * 10, club.Users.Count), Format.Bold(string.Join("\n", result.Skip((curPage) * 10).Take(10))), false)
                         .AddField("_ _", GetText("club_total_invests", String.Format("{0:#,0}", total), Bc.BotConfig.CurrencySign));
                     return embed;
                 }, club.Users.Count, 10, addPaginatedFooter: false);
